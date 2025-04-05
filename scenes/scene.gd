840b0d06d4b0f874c0582a62b41d61b091acdf4d
Natariin <163893047+Natariin@users.noreply.github.com>
2025-04-05 16:57:03 +0200

    Its a picture!

--- scenes/scene.gd
+++ scenes/scene.gd
@@ -1,10 +1,17 @@
 extends Node3D
 
 @onready var scene1 = preload("res://scenes/scene_1.tscn")
+@onready var overlay = $".."
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("scene+"):
 		var new_scene = scene1.instantiate()
 		add_child(new_scene)
+		var tween = create_tween()
+		tween.tween_property(overlay,"position",Vector3(0,0,0),0.5).set_trans(Tween.TRANS_CUBIC)
+		
 	if event.is_action_pressed("scene-"):
+		var tween = create_tween()
+		tween.tween_property(overlay,"position",Vector3(30,0,30),0.5).set_trans(Tween.TRANS_CUBIC)
+		await tween.finished
 		get_child(0).queue_free()
