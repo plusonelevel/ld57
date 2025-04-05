01e1e34661e975bbbea403e45acc639ae4731ee9
Jakub Freywald <jakub.freywald@gmail.com>
2025-04-05 17:52:35 +0200

    finish prototype

---
+++ autoloads/game_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal goal_reached
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	goal_reached.connect(handle_goal_reached)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func handle_goal_reached() -> void:
+	get_tree().quit()
--- scenes/main.gd
+++ scenes/main.gd
@@ -3,11 +3,6 @@ extends Node
 @onready var front_cam := $FrontCamera
 @onready var side_cam := $SideCamera
 
-@onready var c_box_front := $Node3D/Box/CollisionFront
-@onready var c_column_front := $Node3D/Column/CollisionFront
-@onready var c_plaform_front := $Node3D/Platform/CollisionFront
-@onready var c_point_front := $Node3D/Point/CollisionFront
-
 func _init() -> void:
 	#front_cam.current = true
 	InputListener.camera_toggled.connect(toggle_camera)
@@ -15,13 +10,9 @@ func _init() -> void:
 func toggle_camera():
 	if front_cam.current:
 		side_cam.make_current()
-		c_box_front.disabled = true
-		c_column_front.disabled = true
-		c_plaform_front.disabled = true
-		c_point_front.disabled = true
+		for collider in get_tree().get_nodes_in_group("2d_collider"):
+			collider.disabled = true
 	else:
 		front_cam.make_current()
-		c_box_front.disabled = false
-		c_column_front.disabled = false
-		c_plaform_front.disabled = false
-		c_point_front.disabled = false
+		for collider in get_tree().get_nodes_in_group("2d_collider"):
+			collider.disabled = false
