aad710c2fc9d5e45beb5fc01b24f580f187a30ba
Natariin <163893047+Natariin@users.noreply.github.com>
2025-04-07 21:21:18 +0200

    SFX for jump, fall and run

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -3,6 +3,10 @@ extends CharacterBody3D
 @onready var body: MeshInstance3D = $Body
 @onready var state_machine = $Body/AnimationTree["parameters/playback"]
 @onready var is_jumping = false
+@onready var steps: AudioStreamPlayer = $AudioManager/Steps
+@onready var jump: AudioStreamPlayer = $AudioManager/Jump
+@onready var fall: AudioStreamPlayer = $AudioManager/Fall
+
 
 const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
@@ -28,15 +32,17 @@ func _physics_process(delta: float) -> void:
 		if not is_jumping:
 			is_jumping = true
 			state_machine.travel("Jump")
+			jump.play()
 	else:
 		if is_jumping:
 			is_jumping = false
-			# Play landing or idle animation later if needed
+			fall.play()
 
 	# Handle jump input
 	if Input.is_action_just_pressed("up") and is_on_floor() and move_mode == MOVE_MODE.side:
 		velocity.y = JUMP_VELOCITY
 		state_machine.travel("Jump")
+		jump.play()
 		is_jumping = true
 
 	var direction: Vector3
@@ -48,14 +54,20 @@ func _physics_process(delta: float) -> void:
 		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if direction:
-		if not is_jumping:
+		if not is_jumping and is_on_floor():
 			state_machine.travel("Run")
+			if not steps.playing:
+				steps.play()
+		else:
+			steps.stop()
+
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		last_direction = direction
 	else:
 		if not is_jumping:
 			state_machine.travel("Idle")
+			steps.stop()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
